47172471bf92cb4b6a8ebcf0080a03bfc15ca475
yuuto onga <yuuto.onnga.23@gmail.com>
2017-10-08 00:37:54 +0900

    second

--- affine.c
+++ affine.c
@@ -5,15 +5,14 @@
 typedef struct{
     char* magic[3];
     int row,column,max;
-    char*** array
+    char*** array;
 } image_t;
 
-image_t* read_image(char* input)
+int read_image(char* input,image_t* img)
 {
     int i,j,k;
     char* buffer;
     FILE* fp;
-    image_t* img;
 
     buffer=calloc(BUFSIZ,sizeof(char*));
     sprintf(buffer,"./%s",input);
@@ -26,48 +25,78 @@ image_t* read_image(char* input)
     for(i=0;i<3;i++){
         fgets(buffer,BUFSIZ,fp);
         if(i==0)
-            sprintf(img->magic,"%s",buffer);
+            sprintf(img->magic,"%s",(strtok(buffer,"\n")));
         if(i==1){
             img->row=atoi(strtok(buffer," "));
             img->column=atoi(strtok(NULL," "));
         }
         if(i==2)
             img->max=atoi(strtok(buffer," "));
-    }
-
+    } 
     //memory array
-    img->array=malloc(3*sizeof(char**));
+    img->array=malloc(3*sizeof(unsigned char**));
     for(i=0;i<3;i++){
-        img->array[i]=malloc(img->column*sizeof(char*));
+        img->array[i]=malloc(img->column*sizeof(unsigned char*));
         for(j=0;j<(img->column);j++){
-            img->array[i][j]=malloc(img->row*sizeof(char));
+            img->array[i][j]=malloc(img->row*sizeof(unsigned char));
         }
     }
     for(j=0;j<(img->column);j++){
         for(k=0;k<(img->row);k++){
-            fread(img->array[0][j][k],sizeof(unsigned char),1,fp);
-            fread(img->array[1][j][k],sizeof(char),1,fp);
-            fread(img->array[2][j][k],sizeof(char),1,fp);
+            fread(&(img->array[0][j][k]),sizeof(unsigned char),1,fp);
+            fread(&(img->array[1][j][k]),sizeof(unsigned char),1,fp);
+            fread(&(img->array[2][j][k]),sizeof(unsigned char),1,fp);
         }
     }
-    printf("hoge\n");
 
-    return &img;
+    return 0;
 
 }//end of read_image
 
+int make_image(char* out,image_t* img)
+{
+    int i,j;
+    char* buffer;
+    FILE* fp;
+
+    buffer=calloc(BUFSIZ,sizeof(char*));
+    sprintf(buffer,"./%s",out);
+    if((fp=fopen(buffer,"wb"))==NULL){
+        fprintf(stderr,"出力ファイルが開けません.\n");
+        exit(1);
+    }
+    fprintf(fp,"P6\n");
+    fprintf(fp,"%d %d\n",img->row,img->column);
+    fprintf(fp,"%d\n",img->max);
+    for(i=0;i<img->column;i++){
+        for(j=0;j<img->row;j++){
+            fwrite(&img->array[0][i][j],sizeof(unsigned char),1,fp);
+            fwrite(&img->array[1][i][j],sizeof(unsigned char),1,fp);
+            fwrite(&img->array[2][i][j],sizeof(unsigned char),1,fp);
+        }
+    }
+    return 0;
+}//end of make_image
+
 int main(int argc, char** argv)
 {
-    image_t* img;
+    int i;
+    image_t img;
 
     if(argc<3){
         fprintf(stderr,"引数が足りません、入力ファイルと出力ファイルは必須です.\n");
         exit(1);
     }
     
-    img=read_image(argv[1]);
-    printf("%d\n",img->array[2][0][0]);
-
+    read_image(argv[1],&img);
+    printf("hoge\n");
+    printf("%d\n",img.row);
+    printf("%s\n",img.magic);
+    for(i=0;i<10;i++)
+        printf("%d\n",img.array[0][0][i]);
+        printf("%d\n",img.array[1][0][i]);
+        printf("%d\n",img.array[2][0][i]);
+    make_image(argv[2],&img);
 
     return 0;
 }
